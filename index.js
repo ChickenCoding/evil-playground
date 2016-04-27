240a78fad4340b1bd7b010795acdcbc30de5ab2d
ChickenCoding <wardandsmooke@laposte.net>
2016-04-27 21:33:15 +0200

    Adds maybe monad example

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ import log  from './lib/log'
 import R    from 'ramda'
 
 log('Up and running')
+
+export function Maybe(value) {
+  const Nothing = {
+    bind() { return this },
+    maybe(def) { return 0 }
+  }
+  const Just =  {
+    bind(fn) { return Maybe(fn(value)) },
+    maybe(def, fn) { return fn ? fn(def) : def }
+  }
+  return value ? Just : Nothing
+}
+
+const data = [
+  { x: 1, y: 4 },
+  { x: 2 },
+  { x: 3, y: 5 }
+]
+
+function may({x, y}) {
+  return { x: Maybe(x).maybe(x),
+    y: Maybe(y).maybe(y) }
+}
+
+const mayb = data.map(may)
+log(mayb)
